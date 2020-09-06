2d54deae426e4e0751374d3a0283459aa5880ea1
ypk <yprasannakumar@gmail.com>
2020-09-06 15:31:37 +0100

    added babel support

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import App from './App.svelte';
+import "core-js";
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.getElementById("root"),
-	props: {
-		name: 'world'
-	}
+  target: document.getElementById("root"),
+  props: {
+    name: "world",
+  },
 });
 
 window.app = app;
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,66 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
-const sveltePreprocess = require('svelte-preprocess');
-const mode = process.env.NODE_ENV || 'development';
-const prod = mode === 'production';
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const sveltePreprocess = require("svelte-preprocess");
+const mode = process.env.NODE_ENV || "development";
+const prod = mode === "production";
 
 module.exports = {
-	entry: {
-		bundle: ['./src/main.js']
-	},
-	resolve: {
-		alias: {
-			svelte: path.resolve('node_modules', 'svelte')
-		},
-		extensions: ['.mjs', '.js', '.svelte'],
-		mainFields: ['svelte', 'browser', 'module', 'main']
-	},
-	output: {
-		path: __dirname + '/public',
-		filename: '[name].js',
-		chunkFilename: '[name].[id].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svelte$/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						emitCss: true,
-						hotReload: true,
-						preprocess: sveltePreprocess({ postcss: true })
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					/**
-					 * MiniCssExtractPlugin doesn't support HMR.
-					 * For developing, use 'style-loader' instead.
-					 * */
-					prod ? MiniCssExtractPlugin.loader : 'style-loader',
-					'css-loader'
-				]
-			}
-		]
-	},
-	mode,
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		})
-	],
-	devtool: prod ? false: 'source-map'
+  entry: {
+    bundle: ["./src/main.js"],
+  },
+  resolve: {
+    alias: {
+      svelte: path.resolve("node_modules", "svelte"),
+    },
+    extensions: [".mjs", ".js", ".svelte"],
+    mainFields: ["svelte", "browser", "module", "main"],
+  },
+  output: {
+    path: __dirname + "/public",
+    filename: "[name].js",
+    chunkFilename: "[name].[id].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs)$/,
+        exclude: /core-js/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.svelte$/,
+        include: /src|svelte/,
+        use: [
+          "babel-loader",
+          {
+            loader: "svelte-loader",
+            options: {
+              preprocess: sveltePreprocess({ postcss: true }),
+              emitCss: true,
+              hotReload: true,
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          /**
+           * MiniCssExtractPlugin doesn't support HMR.
+           * For developing, use 'style-loader' instead.
+           * */
+          prod ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+        ],
+      },
+    ],
+  },
+  mode,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  devtool: prod ? false : "source-map",
 };
