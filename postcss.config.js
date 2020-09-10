3d9cddaab8c86361da646066804ee46ec7aa0ede
ypk <yprasannakumar@gmail.com>
2020-09-10 21:51:55 +0100

    refactored css location added tailwind.css to inject tailwind base component and utils updated postcss config updated package.json with new build script

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+    content: ["./views/**/*.ejs"],
+    defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  });
+  
+  module.exports = {
+    plugins: [
+      require("tailwindcss"),
+      require("autoprefixer"),
+   //   purgecss
+    ],
+  };
