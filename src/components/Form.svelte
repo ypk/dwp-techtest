c3e912bbf757fdbdf365c1900286cc92103c032e
ypk <yprasannakumar@gmail.com>
2020-10-14 13:02:04 +0100

    updated form UI based on node design refactored result logic to render correct ui

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -69,30 +69,39 @@
   }
 </script>
 
-<h1 class="mb-12 text-4xl">Distance Calculator</h1>
-<div class="mb-12">
-  <p>
-    This form fetches a list of users who live within a specified distance from
-    London City.
+<h1 class="mb-4 text-4xl md:mb-12 md:text-6xl leading-tight">
+  Distance Calculator
+</h1>
+<div class="mb-6 text-lg md:mb-12 md:text-xl">
+  <p class="py-2 leading-loose">
+    This form fetches a list of users from a specific API and displays who live
+    within a
+    <br />user-specified distance from
+    <span
+      class="text-white leading-none cursor-text font-semibold bg-gray-800 rounded px-2 py-1"
+      title="<%-'Latitude: '+initialCoords.latitude+', Longitude: '+initialCoords.longitude %>">London
+      City</span>
   </p>
-  <p id="role-help">Please enter the radius in miles to begin the search</p>
+  <p class="py-2" id="role-help">
+    Please enter the radius in miles to begin the search
+  </p>
 </div>
 <form
   id="distance-calculator"
   name="distance-calculator"
   on:submit|preventDefault={handleSubmit}
-  on:reset|preventDefault={handleReset}>
-  <div class="flex flex-wrap" novalidate>
-    <div class="mb-12 w-full">
+  on:reset|preventDefault={handleReset}
+  novalidate>
+  <div class="flex flex-wrap">
+    <div class="mb-6 md:mb-12 w-full">
       <label
-        class="block uppercase tracking-wide text-gray-700 text-xs font-bold
-          mb-2"
-        for="grid-password">
-        <span>Distance</span>
+        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+        for="distance">
+        <span class="text-sm font-normal md:text-lg">Distance</span>
         <input
           class="appearance-none block w-full bg-gray-100 text-gray-700 border
-            border-gray-500 rounded py-3 px-4 my-3 leading-tight
-            focus:outline-none focus:bg-white focus:border-gray-700 {errors.distance ? 'border-red-700' : ''}"
+          border-gray-500 rounded py-3 px-4 my-3 text-lg leading-tight focus:outline-none
+          focus:bg-white focus:border-gray-700 {errors.distance ? 'border-red-700' : ''}"
           id="distance"
           name="distance"
           type="text"
@@ -101,24 +110,21 @@
           placeholder="Distance (in miles) e.g.: 20" />
       </label>
       {#if errors.distance}
-        <p aria-live="polite" class="text-red-700 text-xs italic">
+        <p aria-live="polite" class="text-red-700 text-lg md:text-xl italic">
           {errors.distance}
         </p>
       {/if}
     </div>
-    <div class="w-full">
+    <div class="w-full flex flex-col md:flex-row">
       <button
-        class="flex-shrink-0 bg-black hover:bg-black border-black
-          hover:bg-gray-800 hover:border-gray-800 text-sm border-4 text-white
-          py-1 px-2 rounded"
+        class="font-bold bg-black hover:bg-black border-black text-lg hover:bg-gray-800 hover:border-gray-800 text-sm border-4 text-white py-1 px-4 rounded mb-6 md:mb-auto"
         aria-label="Calculate Distance"
         role="button"
         type="submit">
         Calculate Distance
       </button>
       <button
-        class="flex-shrink-0 border-transparent border-4 text-black
-          hover:text-gray-500 text-sm py-1 px-2 rounded"
+        class="flex-shrink-0 font-bold border-transparent border-4 text-black text-lg hover:text-gray-500 text-sm py-1 px-4 rounded"
         aria-label="Reset Form"
         role="button"
         type="reset">
--- src/components/Result.svelte
+++ src/components/Result.svelte
@@ -1,18 +1,43 @@
 <script>
   import { responseData } from "../common";
+  $: div =
+    $responseData && ($responseData.isLoading || $responseData.hasLoaded) 
+      ? "my-12 border rounded-md shadow-md px-6 pt-3 pb-6 text-xl"
+      : "";
 </script>
 
-<div class="my-12" aria-live="polite" aria-busy={$responseData.isLoading ? "true" : "false"}>
+<div
+  class={'my-12 px-6 pt-3 pb-6 text-xl' + div}
+  aria-live="polite"
+  aria-busy={$responseData.isLoading ? 'true' : 'false'}>
   {#if $responseData.isLoading}
-    <p>Loading...</p>
-  {:else if !$responseData.foundUsers && $responseData.hasLoaded }
-    <p>No data found!</p>
+    <p class="mt-2 mb-4">Loading...</p>
+  {:else if !$responseData.foundUsers && $responseData.hasLoaded}
+    <p class="mt-2 mb-4">No data found!</p>
+    <a
+      class="py-1 px-4 font-bold bg-black hover:bg-black border-black text-lg hover:bg-gray-800 hover:border-gray-800 text-sm border-4 text-white rounded"
+      aria-label="OK"
+      role="link"
+      href="/">
+      OK
+    </a>
   {:else if !$responseData.isLoading && $responseData.data.length > 0}
-    <p>Users who live {$responseData.distance} miles from London are:</p>
+    <p class="mt-2 mb-4">
+      Users who live
+      {$responseData.distance}
+      miles from London are:
+    </p>
     <ul class="my-6">
       {#each $responseData.data as { id, first_name, last_name, email }, i}
         <li {id}>{first_name} {last_name} - {email}</li>
       {/each}
     </ul>
+    <a
+      class="py-1 px-4 font-bold bg-black hover:bg-black border-black text-lg hover:bg-gray-800 hover:border-gray-800 text-sm border-4 text-white rounded"
+      aria-label="Clear Results"
+      role="link"
+      href="/">
+      Clear Results
+    </a>
   {/if}
 </div>
